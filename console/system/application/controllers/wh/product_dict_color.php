6c0837b95f1aa53428fcc24b947a79aa7e33d74b
Kamil Majewski <biuro@katashi.pl>
2013-01-31 11:27:23 +0100

    Product - Init

---
+++ console/system/application/controllers/wh/product_dict_color.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined('BASEPATH')) die;
+
+class Product_Dict_Color extends Main {
+
+    function Product_Dict_Color($_ci = '') {
+        parent::Controller();
+        //
+        $this->ci = $_ci;
+        // load models
+        $this->load->model('main_model');
+        $this->load->model('wh/product_dict_color_model');
+    }
+
+    // display
+    function display() {
+        $this->ci->smarty->display('wh_admin/product_dict_color.html');
+    }
+    function display_add() {
+        $this->ci->smarty->display('wh_admin/product_dict_color_add.html');
+    }
+
+    // load
+    function load_all() {
+        echo '{"total":'.json_encode($this->product_dict_color_model->load_all_count()).', "data":'.json_encode($this->product_dict_color_model->load_all()).'}';
+    }
+    function load($id = null) {
+        echo '{"success": 1, "data":'.json_encode($this->product_dict_model_model->load($id)).'}';
+    }
+
+    // add
+    function add() {
+        $result = $this->product_dict_color_model->add();
+        echo '{"success": '. $result .'}';
+    }
+
+    // delete
+    function delete($id = null) {
+        $result = $this->product_dict_color_model->delete($id);
+        echo 'grid';
+    }
+
+}
---
+++ console/system/application/models/wh/product_dict_color_model.php
@@ -0,0 +1,45 @@
+<?php
+class Product_Dict_Color_Model extends Main_Model {
+	
+	function Product_Dict_Color_Model() {
+		// Call the Model constructor
+		parent::Model();
+		//
+		if (isset($this->ci)) { $this->db = $this->ci->db; }
+        //
+        $this->table_name = 'wh_product_dict_color';
+	}
+
+    // load
+    function load_all_count() {
+        $this->db->from($this->table_name);
+        return $this->db->count_all_results();
+    }
+    function load_all() {
+        $this->limit_check();
+        $query = $this->db->get($this->table_name);
+        $record = $query->result_array();
+        return $record;
+    }
+    function load($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get($this->table_name);
+        $record = $query->row_array();
+        return $record;
+    }
+
+    // add
+    function add() {
+        $this->db->insert($this->table_name, $_POST);
+        return 1;
+    }
+
+    // delete
+    function delete($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->delete($this->table_name);
+        return 1;
+    }
+
+}
+?>
